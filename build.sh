6435bc347998b638e644c57d1ca53d80c53311be
AlexMiroshnikov <ilex@ilex-thinknote>
2015-09-12 21:19:17 +0700

    Add build.sh

---
+++ build.sh
@@ -0,0 +1,9 @@
+jsx --no-cache-dir -ex jsx ./public/react/app ./public/react/build \
+&& \
+cat \
+./public/react/app/appevent.js \
+./public/react/app/dispatcher.js \
+./public/react/app/model/ModelEntry.js \
+./public/react/build/component/* \
+./public/react/build/js.js \
+| sed -r 's/[\t\r\n]+//' | sed -r 's/ [ ]+//' | tr '\n' ' ' > ./public/react/min.js
